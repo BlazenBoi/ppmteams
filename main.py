71693c86ae505ca89423594ac0a0fd7aa1906868
BlazenBoi <runnerelite0@gmail.com>
2024-03-11 00:04:16 +0000

    Simple Function

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from flask import Flask
+import subprocess
+try:
+    from quart import Quart, jsonify
+    from motor.motor_asyncio import AsyncIOMotorClient
+    import configparser
+except:
+    subprocess.run(["python3", "-m", "pip", "install", "-r", "requirements.txt"])
+    from quart import Quart, jsonify
+    from motor.motor_asyncio import AsyncIOMotorClient
+    import configparser
 
-app = Flask(__name__)
+config = configparser.ConfigParser()
+config.read('config.properties')
+
+app = Quart(__name__)
+app.db = AsyncIOMotorClient(config.get("variables", "mongourl"))[config.get("variables", "database")]
+app.db.pcollection = app.db["ppms"]
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+async def hello_world():
+    ppms = []
+    cursor = app.db.pcollection.find()
+    scursor = cursor.sort("ppmdate", 1)
+    scursorlist = await scursor.to_list(None)
+    for document in scursorlist:
+        ppms.append({"ppmname":document["ppmname"], "ppmdate":document["ppmdate"], "ppmtype":document["ppmtype"], "ppmtimes":document["ppmtimes"], "scores":document["scores"]})
+    return jsonify(ppms)
+
+    
